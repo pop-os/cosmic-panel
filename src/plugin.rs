c45582afc53441642844d5c30a32ac125bb4360d
Ashley Wulber <Ashley@system76.com>
2022-01-24 11:46:00 -0500

    first commit

---
+++ src/plugin.rs
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-only
+use gtk4::glib;
+
+#[derive(Clone, Debug, Default, gtk4::glib::Boxed)]
+#[boxed_type(name = "BoxedDockPlugin")]
+pub struct BoxedDockPlugin {
+    pub path: String,
+    pub name: String,
+    pub image: gtk4::Image,
+    pub popover_menu: gtk4::Box,
+}
