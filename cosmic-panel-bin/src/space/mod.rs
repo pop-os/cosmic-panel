eca5ec4f4cdcd95b4b00ba6d0d9ee6ed1fb4f77a
Ashley Wulber <ashley@system76.com>
2022-07-21 12:04:36 -0400

    docs

--- cosmic-panel-bin/src/space/mod.rs
+++ cosmic-panel-bin/src/space/mod.rs
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: MPL-2.0-only
 
+//! PanelSpace is a container for all running panels, spawning each as a separate process and compositing them in a layer shell surface as configured
+//! PanelSpace implements the WrapperSpace abstraction
+
 mod panel_space;
 mod wrapper_space;
 
