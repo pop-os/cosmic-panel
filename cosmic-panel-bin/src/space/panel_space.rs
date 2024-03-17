a9d980ee10cc2da760ed065ba7424274f2b69f52
Ashley Wulber <48420062+wash2@users.noreply.github.com>
2024-03-16 21:09:16 -0400

    Revert "fix: reset the renderer when we animate the panel"
    This reverts commit adb7bc829e4fb7a151eeb564051b8dbe7adf861d.

--- cosmic-panel-bin/src/space/panel_space.rs
+++ cosmic-panel-bin/src/space/panel_space.rs
@@ -610,14 +610,6 @@ impl PanelSpace {
 
     fn apply_animation_state(&mut self) {
         if let Some(animation_state) = self.animate_state.as_mut() {
-            self.damage_tracked_renderer = Some(OutputDamageTracker::new(
-                self.dimensions
-                    .to_f64()
-                    .to_physical(self.scale)
-                    .to_i32_round(),
-                1.0,
-                smithay::utils::Transform::Flipped180,
-            ));
             self.panel_changed = true;
             let progress = (Instant::now()
                 .duration_since(animation_state.started_at)
