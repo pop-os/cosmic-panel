b83f2700ea0b8368566b638784758a5b3b32e809
Ryan Brue <ryanbrue.dev@gmail.com>
2024-11-02 00:32:45 -0500

    panel-config: Add border_width property
    This commit adds a new property to the panel config, which allows users to specify a border for the panel.
    
    Signed-off-by: Ryan Brue <ryanbrue.dev@gmail.com>

--- cosmic-panel-config/src/container_config.rs
+++ cosmic-panel-config/src/container_config.rs
@@ -158,6 +158,7 @@ impl Default for CosmicPanelContainerConfig {
                     padding: 0,
                     spacing: 2,
                     border_radius: 0,
+                    border_width: 0.0,
                     exclusive_zone: true,
                     autohide: None,
                     margin: 0,
@@ -187,6 +188,7 @@ impl Default for CosmicPanelContainerConfig {
                     padding: 0,
                     spacing: 4,
                     border_radius: 160,
+                    border_width: 0.0,
                     exclusive_zone: false,
                     autohide: Some(crate::AutoHide {
                         wait_time: 500,
