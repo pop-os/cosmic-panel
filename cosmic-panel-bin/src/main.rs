1c9c4e2a2cf27efd0ca77b5ec21bc6f7fa92d9da
Ashley Wulber <ashley@system76.com>
2024-12-06 16:21:30 -0500

    fix: typo

--- cosmic-panel-bin/src/main.rs
+++ cosmic-panel-bin/src/main.rs
@@ -44,7 +44,7 @@ fn main() -> Result<()> {
     let filter_layer =
         EnvFilter::try_from_default_env().or_else(|_| EnvFilter::try_new("warn")).unwrap();
     if let Ok(journal_layer) = tracing_journald::layer() {
-        tracing_subscriber::registry().with(fmt_layer).with(filter_layer).init();
+        tracing_subscriber::registry().with(journal_layer).with(filter_layer).init();
     } else {
         tracing_subscriber::registry().with(fmt_layer).with(filter_layer).init();
     }
